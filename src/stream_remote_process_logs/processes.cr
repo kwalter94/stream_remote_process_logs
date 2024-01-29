b238877aa929d3af17829e98fa670b2a835dbc1a
Walter Kaunda <walterkaunda@gmail.com>
2024-01-29 21:54:08 +0200

    Fixes... Fixes ... Fixes...

--- src/stream_remote_process_logs/processes.cr
+++ src/stream_remote_process_logs/processes.cr
@@ -4,16 +4,21 @@ module StreamRemoteProcessLogs
   module Processes
     def self.run_process(command : String, args : Array(String))
       process_id = UUID.v4.to_s
+      puts "Starting new process: #{process_id}"
 
       Process.run(command, args) do |process|
         redis = Redis.new
-        channel_id = "commands:#{process_id}"
+        channel_id = command_channel_id(process_id)
         puts("Publishing to channel: #{channel_id}")
 
         process.output.each_line do |line|
           redis.publish(channel_id, line)
         end
       end
+    end
+
+    def self.command_channel_id(process_id : String) : String
+      "commands:#{process_id}"
     end
   end
 end
--- src/stream_remote_process_logs/web.cr
+++ src/stream_remote_process_logs/web.cr
@@ -1,17 +1,21 @@
 require "kemal"
 require "redis"
 
+require "./processes"
+
 module StreamRemoteProcessLogs
   module Web
     get "/" do
-      render "src/views/index.ecr"
+      render "src/stream_remote_process_logs/views/index.ecr"
     end
 
-    ws "/command/:id" do |socket, context|
-      command_id = context.ws_route_lookup.params["id"]
+    ws "/process/:id" do |socket, context|
+      process_id = context.ws_route_lookup.params["id"]
+      channel_id = Processes.command_channel_id(process_id)
 
       redis = Redis.new
-      redis.subscribe(command_id) do |on|
+      puts "Connecting to channel: #{channel_id}"
+      redis.subscribe(channel_id) do |on|
         on.message do |_channel, message|
           socket.send({ message: message }.to_json)
         end
